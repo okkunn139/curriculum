680264ce8e0140065ea160514cd0b49c49e87e4f
okkunn139 <okujuny@gmail.com>
2023-08-22 02:01:42 +0900

    8-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -11,7 +11,17 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
+    public function create() {
+        return view('posts.create');
+    }
+    
     public function show(Post $post) {
         return view('posts.show')->with(['post' => $post]);
+    }
+    
+    public function store(PostRequest $request, Post $post) {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
